414291756fa4b515b58dd7a6b25cfe30376b0aec
Thomas Westermann <thomas.westermann@orange.fr>
2020-01-12 14:55:15 +0100

    Added Logout

--- resources/user.py
+++ resources/user.py
@@ -5,9 +5,12 @@ from flask_jwt_extended import (
     create_access_token, 
     create_refresh_token,
     jwt_refresh_token_required,
-    get_jwt_identity
+    get_jwt_identity,
+    jwt_required,
+    get_raw_jwt
 )
 from models.user import UserModel
+from blacklist import BLACKLIST
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username',
@@ -67,6 +70,12 @@ class UserLogin(Resource):
 
         return {'message': 'Invalid credentials'}, 401
 
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        BLACKLIST.add(jti)
+        return {'message': 'Successfully Logged out'}
 
 class TokenRefresh(Resource):
     @jwt_refresh_token_required
